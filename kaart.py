ee21f9231fb385f127443a7cdfc19734ce831158
vandewsa <6703S4200v>
2022-08-18 14:21:39 +0200

    Additions

--- kaart.py
+++ kaart.py
@@ -2,11 +2,20 @@ import pandas as pd
 import plotly.express as px
 
 #dataframe maken met info uit csv
-df_straten = pd.read_csv(r"C:\Users\vandewsa\Downloads\Fotocollectie Industriemuseum- Beluiken.csv")
+df_straten = pd.read_csv(r"C:\Users\vandewsa\Downloads\Fotocollectie Industriemuseum_Beluiken.csv")
 
 #kaart creeëren
-fig = px.scatter_mapbox(df_straten, lat="lat", lon="lon", hover_name="straat", hover_data=["wijk", "aantal"],
-                        color_discrete_sequence=["#E8BE19"], size="aantal", zoom=11, height=800)
+fig = px.scatter_mapbox(df_straten,
+                        lat="lat",
+                        lon="lon",
+                        hover_name="straat",
+                        hover_data=["wijk", "aantal"],
+                        title="Beluiken fotocollectie Industriemuseum",
+                        color="aantal",
+                        size="aantal",
+                        zoom=11,
+                        height=800
+                        )
 fig.update_layout(mapbox_style="open-street-map")
 fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 fig.show()
